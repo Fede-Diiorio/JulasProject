2cb1cb09a8493c188a9861a28e2d52cd3de96acc
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-28 17:56:55 -0300

    Se comenzó a incorporar firabase al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Accounts from './components/Accounts/Accounts'
+import Index from './components/Index/Index'
 import SideBar from './components/SideBar/SideBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <SideBar />
-      <Accounts title={'Mis cuentas'} />
-    </>
+    <main>
+      <BrowserRouter>
+        <SideBar />
+        <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='/category/:categoryId' element={<Accounts title={"Titulo"} />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   )
 }
 
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,36 +1,24 @@
 import classes from './SideBar.module.scss';
-import { FaHouseChimney } from "react-icons/fa6";
-import { IoDocumentText } from "react-icons/io5";
-import { FaTruck } from "react-icons/fa";
-import { BsQuestionCircleFill } from "react-icons/bs";
-import { BsFillLightningChargeFill } from "react-icons/bs";
+import { getCategories } from '../../services/firebase/firestore/categories';
+import { useAsync } from '../../hooks/useAsync';
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
+
+    const asyncFunctiron = () => getCategories();
+    const { data: categories } = useAsync(asyncFunctiron, []);
+    console.log(categories);
+
     return (
         <aside className={classes.aside}>
             <h2 className={classes.title}>Edea</h2>
             <nav>
                 <ul>
-                    <li className={classes.field}>
-                        <FaHouseChimney className={classes.icon} />
-                        <h4>Mis cuentas</h4>
-                    </li>
-                    <li className={classes.field}>
-                        <IoDocumentText className={classes.icon} />
-                        <h4>Trámites</h4>
-                    </li>
-                    <li className={classes.field}>
-                        <FaTruck className={classes.icon} />
-                        <h4>Servicio técnico</h4>
-                    </li>
-                    <li className={classes.field}>
-                        <BsQuestionCircleFill className={classes.icon} />
-                        <h4>Consultar segmentación tarifaria</h4>
-                    </li>
-                    <li className={classes.field}>
-                        <BsFillLightningChargeFill className={classes.icon} />
-                        <h4>Simulador</h4>
-                    </li>
+                    {
+                        categories.map(cat => (
+                            <li key={cat.id}><Link to={`category/${cat.slug}`}>{cat.name}</Link></li>
+                        ))
+                    }
                 </ul>
             </nav>
         </aside>
