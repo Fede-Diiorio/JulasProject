aa22a49161751c72715ddb33b515cc86c7bad9d5
Federico Di Iorio <federico_metallica@hotmail.com>
2024-03-05 17:07:52 -0300

    Se creó el componente AccountState

--- src/components/Accounts/Accounts.jsx
+++ src/components/Accounts/Accounts.jsx
@@ -1,13 +1,17 @@
 import classes from './Accounts.module.scss'
 import Taxes from '../Taxes/Taxes';
 import House from '../House/House'
+import AccountState from '../AccountState/AccountState';
 
 const Accounts = ({ title }) => {
     return (
         <div className={classes.itemContainer}>
             <h2 className={classes.title}>{title}</h2>
             <House />
-            <Taxes title={'Facturas'} state={'Cobrada'} date={'22 de Diciembre'} price={'$ 6.850,45'} />
+            <div className={classes.gridContainer}>
+                <Taxes title={'Facturas'} state={'Cobrada'} date={'22 de Diciembre'} price={'$ 6.850,45'} />
+                <AccountState />
+            </div>
         </div>
     )
 };
